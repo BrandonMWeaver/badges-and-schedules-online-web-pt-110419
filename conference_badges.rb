19c7fed32daf8bda0cf6775c327d15ff761bdac3
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 01:28:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,20 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(names)
   temp = []
-  array.each do |name|
+  names.each do |name|
     temp << "Hello, my name is #{name}."
   end
   return temp
 end
 
-def assign_rooms(array)
-  
+def assign_rooms(speakers)
+  room_assignments = []
+  room = 1
+  speakers.each do |speaker|
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  return room_assignments
 end
