2d55adb9f90ce5533b7b1240b5e781c595decaab
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 01:25:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(list)
-  
+def batch_badge_creator(array)
+  temp = []
+  array.each do |name|
+    temp << "Hello, my name is #{name}."
+  end
+  return temp
 end
